a121921bd3e7a0d53730d1a61fb01e8439cd3f07
Davide Radaelli <daviderady@gmail.com>
2021-05-06 15:04:13 -0400

    Added all the pages

--- Pages/diveroll.md
+++
@@ -1,5 +0,0 @@
-# Dive Roll
-Gotta dive then rolll
-
-Check out these videos for more details:
-https://www.youtube.com/watch?v=KPtK7q5bNLA
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::fs;
+use glob::glob;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let mut skills: Vec<String> = vec![];
+    for path in glob("./Packages/**/*.svg").expect("Failed at glob") {
+        dbg!(&path);
+        let mut svg = fs::read_to_string(path.unwrap()).unwrap();
+
+        // Remove all <span> tags
+        svg = svg.replace(r"<span>", "");
+        svg = svg.replace(r"</span>", "");
+
+        let mut sliced = svg.split(r"<rect");
+
+        sliced.next();
+
+        let sliced: Vec<_> = sliced.collect();
+
+        for slice in sliced {
+            let slice = slice.to_string();
+            // find skill
+            let mut search_domain = slice.to_string().clone();
+
+            // closer to answer 1
+            let from = search_domain.find("word-wrap").unwrap();
+            search_domain = search_domain[from..].to_string();
+
+            let from2 = search_domain.find(">").unwrap();
+            let to = search_domain.find("<").unwrap();
+
+            let skill_exact = search_domain[from2 + 1..to].to_string();
+
+            let skill = skill_exact
+                .split_whitespace()
+                .collect::<String>()
+                .chars()
+                .filter(|c| c.is_alphanumeric())
+                .collect::<String>()
+                .to_lowercase();
+
+
+            // skip if empty
+            if skill == "".to_string() {
+                println!("Skipped empty box");
+                continue;
+            }
+
+            skills.push(skill);
+        }
+    }
+
+    for skill in skills {
+        let path = format!("./Pages/{}.md", &skill);
+        if !Path::new(&path).exists() {
+            let default = fs::read_to_string("./default.md").unwrap();
+
+            fs::write(&path, &default).unwrap();
+        }
+    }
 }
